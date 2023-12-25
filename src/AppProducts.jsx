82de4fb88a95b998ba6e3264e847ba8fd4ced7bf
noaprost <kangjiwon1850@gmail.com>
2023-12-25 15:46:33 +0900

    useEffect learn

--- src/AppProducts.jsx
+++ src/AppProducts.jsx
@@ -3,9 +3,13 @@ import Products from "./components/Products";
 
 export default function AppProducts() {
   const [showProducts, setShowProducts] = useState(true);
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((prev) => !prev);
   return (
     <>
-      {showProducts && <Products />}
+      {showProducts && (
+        <Products checked={checked} handleChange={handleChange} />
+      )}
       <button
         onClick={() => {
           setShowProducts((show) => !show);
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 
-export default function Products() {
+export default function Products({ checked, handleChange }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("data/products.json")
+    fetch(`data/${checked ? "sale_" : ""}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log("success");
@@ -13,12 +13,21 @@ export default function Products() {
     return () => {
       console.log("clear");
     };
-  }, []);
+  }, [checked]);
 
   return (
     <>
+      <input
+        id="checkbox"
+        type="checkbox"
+        checked={checked}
+        onChange={handleChange}
+      />
+      <label htmlFor="checkbox">Only Show Sale!</label>
       <ul>
         {products.map((product) => (
+          // map의 item을 기반으로 자식을 만들때는 꼭 고유한 key 값을 정해줘야함
+          // 이 key를 보고 업데이트 필요 유무와 배열에서의 순서등을 판단하기 떄문
           <li key={product.id}>
             <article>
               <h3>{product.name}</h3>
