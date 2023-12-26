b6d34e6e0850338ac097f955bb21acfd92864cea
noaprost <kangjiwon1850@gmail.com>
2023-12-26 16:59:52 +0900

    useReducer 사용해보기

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import AppMentors from "./AppMentors";
+import AppMentorsUseReducer from "./AppMentorsUseReducer";
 
 // 리액트에서 시작 포인터가 되는 부분
 // root라는 id를 요소를 가져와서 거기에 루트를 만들어줌
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // 개발할 때는 StrictMode에서 조금더 엄격하게 개발할 수 있도록 하고, 배포할 때는 이 모드가 자동으로 꺼짐
   <React.StrictMode>
-    <AppMentors />
+    <AppMentorsUseReducer />
   </React.StrictMode>
 );
 
